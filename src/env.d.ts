21de48a3c37641da1a5133b32b1deb80697ef070
Yuhang Chen <i@yuhang.ch>
2024-04-01 17:16:24 +0800

    :beers: disable some personal features.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
-interface ImportMetaEnv {
-    readonly BLOG_API_SECRET: string
-    readonly BLOG_API: string
-}
-
 interface ImportMeta {
     readonly env: ImportMetaEnv
 }
